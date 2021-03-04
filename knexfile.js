e0497419b3f8e2ec0e38c212c5586dfa2c5b1ef3
Max Krieg <maxwell.krieg@gmail.com>
2021-03-03 19:22:10 -0700

    first commit

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+require('ts-node/register');
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: 'postgresql://postgres:postgres@127.0.0.1/cohostdb'
+  },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
---
+++ migrations/20210303090630_create-users.js
@@ -0,0 +1,15 @@
+exports.up = async (knex) => {
+  await knex.raw('create extension if not exists "uuid-ossp"');
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('email').notNullable();
+    table.uuid('handle').defaultTo(knex.raw('uuid_generate_v4()'));
+    table.string('firstName');
+    table.string('lastName');
+    table.string('password').notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('users');
+};
